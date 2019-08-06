ecabc7581a0864fb51b31bff3283e3095e9e9454
admia1 <sampadpars@gmail.com>
2019-08-06 12:44:04 +0430

    debuging + now working hello world

--- registeration/urls.py
+++ registeration/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'registeration'
 
 urlpatterns = [
-    path('hello_world/', views.hello_world, nane="hello"),
+    path('hello_world/', views.hello_world, name="hello"),
 ]
