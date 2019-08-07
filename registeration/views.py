9abd1bc3a6756e03535d97fb4d1fa65a14b3bf79
admia1 <sampadpars@gmail.com>
2019-08-07 19:05:06 +0430

    not sure about message

--- registeration/views.py
+++ registeration/views.py
@@ -7,8 +7,6 @@ from django.contrib.auth.models import User
 
 from .models import Person
 
-def register_post_validator(post):
-    return True
 
 def farsi_to_english_digit(number_string):
     dic = {
@@ -27,6 +25,67 @@ def farsi_to_english_digit(number_string):
         number_string = number_string.replace(digit, dic[digit])
     return number_string
 
+
+def register_post_validator(post):
+    def string_type_validator(field_name, field_max_length):
+        if field_name not in post:
+            return "field %s is missing"%field_name
+        if len(post[field_name]) < 2 :
+            return "field %s is too short"%field_name
+        if len(post[field_name]) > field_max_length :
+            return "field %s is too long"%field_name
+        return ""
+
+    for checking_field in ["first_name", "last_name", "national_id", "phone_number","email",]:
+        res = string_type_validator(checking_field, 99)
+        if res:
+            return res
+    ### missing check email $$$
+    if len(post['national_id']) != 10:
+        return "short or long national_id"
+
+    for digit in post["national_id"]:
+        if digit not in "0123456789۰۱۲۳۴۵۶۷۸۹":
+            return "non numerical national_id"
+
+    for digit in post['phone_number']:
+        if digit not in "0123456789۰۱۲۳۴۵۶۷۸۹+":
+            return "non numerical phone_number"
+
+    if post['detail_type'] not in ['0','1','2','3','4']:
+        return "bad format of detail_type"
+
+    type = int(post['detail_type'])
+
+    if type == 0:
+        res = string_type_validator("guide_id", 99)
+        if res:
+            return res
+
+    elif type == 1:
+        for checking_field in ["university", "study_field", "student_id"]:
+            res = string_type_validator(checking_field, 99)
+            if res:
+                return res
+        for digit in post["student_id"]:
+            if digit not in "0123456789۰۱۲۳۴۵۶۷۸۹":
+                return "bad student_id format"
+
+    elif type == 2:
+        for checking_field in ["agency", "city",]:
+            res = string_type_validator(checking_field, 99)
+            if res:
+                return res
+
+    elif type == 3:
+        for checking_field in ["hotel", "city",]:
+            res = string_type_validator(checking_field, 99)
+            if res:
+                return res
+
+    return ""
+
+
 def register_view(request):
     template = 'registeration/register.html'
     if request.user.is_authenticated:
@@ -34,11 +93,13 @@ def register_view(request):
     else:
         if request.method == 'POST':
             #validation of data
-            if register_post_validator(request.POST):
+            error_message = register_post_validator(request.POST)
+            if error_message:
                 # if user didnt registered before
-                if Person.objects.filter(national_id = farsi_to_english_digit(request.POST['national_id'])).exists():
+                eng_national_id = farsi_to_english_digit(request.POST['national_id'])
+                if ~(Person.objects.filter(national_id = eng_national_id).exists()):
                     # then create the user
-                    user = User.object.Create_user(
+                    user = User(
                         username = request.POST['email'],
                         password = farsi_to_english_digit(request.POST['national_id']),
                     )
@@ -73,16 +134,16 @@ def register_view(request):
                     return HttpResponseRedirect(reverse('registeration:home'))
 
                 else:
-                    return(request,template,{'error_message': "you,ve already registered!"})
+                    return render(request,template,{'error_message': "you,ve already registered"})
             else:
-                return(request,template,{'error_message': "invalid data"})
+                return render(request,template,{'error_message': error_message})
         else:
             return render(request,template)
 
 def login_view(request):
     template = 'registeration/login.html'
     if request.user.is_authenticated :
-        return HttpResponseRedirect(reverse('polls:home'))
+        return HttpResponseRedirect(reverse('registeration:home'))
 
     if request.method == 'POST':
         if 'username' in request.POST and 'password' in request.POST:
@@ -92,7 +153,7 @@ def login_view(request):
                     login(request, user)
                     return HttpResponseRedirect(reverse('polls:home'))
 
-        return render(request, template, {'error_message': 'Wrong username or password!'})
+        return render(request, template, {'error_message': 'Wrong username or password'})
 
     return render(request, template)
 
