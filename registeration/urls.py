e3989acbc67d3ab2426ccebb6580b65c12b12b3d
admia1 <sampadpars@gmail.com>
2019-08-08 20:55:55 +0430

    static steel dont work

--- registeration/urls.py
+++ registeration/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('register/', views.register_view, name="register"),
     path('login/', views.login_view, name="login"),
     path('home/' , views.home_view, name="home"),
-    url('', views.show_off_view, name="show_off")
+    path('index/', views.show_off_view, name="show_off")
 ]
