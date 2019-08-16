c31d06e87907cdd1331f32be47615ddd8c985fc8
admia1 <sampadpars@gmail.com>
2019-08-16 20:42:14 +0430

    adding discount to admin

--- registeration/admin.py
+++ registeration/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Event, Invoice, Person
+from .models import Event, Invoice, Person, Discount
 # Register your models here.
 
 
 admin.site.register(Event)
 admin.site.register(Invoice)
 admin.site.register(Person)
+admin.site.register(Discount)
