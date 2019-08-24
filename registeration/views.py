12d57945b58c53f6cfb2ef2addd010d02b6ed5b9
Ali Mahdiyar <alimahdiyar77@gmail.com>
2019-08-24 13:31:15 +0430

    bug fix

--- registeration/views.py
+++ registeration/views.py
@@ -57,7 +57,7 @@ def register_post_validator(post):
         if digit not in "0123456789۰۱۲۳۴۵۶۷۸۹+":
             return "non numerical phone_number"
 
-    if post['detail_type'] not in ['0','1','2','3',]:
+    if post['detail_type'] not in ['0','1','2','3', '4']:
         return "bad format of detail_type"
 
     type = int(post['detail_type'])
